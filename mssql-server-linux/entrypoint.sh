549cd4ef5c54d57bb9537ce05235dec8653a34c9
Vasily Chernov <Vasily.Chernov@jetbrains.com>
2017-08-15 17:27:53 +0300

    [mssql] fix increment issue on ms sql server start up

--- mssql-server-linux/entrypoint.sh
+++ mssql-server-linux/entrypoint.sh
@@ -7,7 +7,7 @@ export STATUS=0
 i=0
 while [[ $STATUS -eq 0 ]] || [[ $i -lt 30 ]]; do
 	sleep 1
-	i=$i+1
+	i=$((i+1))
 	STATUS=$(grep 'Server setup is completed' /var/opt/mssql/log/setup*.log | wc -l)
 done
 
