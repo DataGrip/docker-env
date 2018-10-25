765c9a58641500e1391f1e5e90e3606893299026
Vasily Chernov <Vasily.Chernov@jetbrains.com>
2018-10-25 18:38:51 +0300

    [dbe] Introducing enchanced Cassandra 2.1

---
+++ cassandra/2.1/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+echo =============== WAITING FOR CASSANDRA ==========================
+#waiting for CASSANDRA to start
+export STATUS=0
+i=0
+while [[ $STATUS -eq 0 ]] || [[ $i -lt 90 ]]; do
+	sleep 1
+	i=$((i+1))
+	STATUS=$(grep 'Starting listening for CQL clients' /var/log/cassandra/system.log | wc -l)
+done
+
+echo =============== CASSANDRA STARTED ==========================
+
+if [ ! -z $KEY_SPACE ]; then
+	echo "KEY_SPACE: $KEY_SPACE"	
+else
+	KEY_SPACE=guest
+	echo "KEY_SPACE: $KEY_SPACE"
+fi
+
+cd /tmp/
+cat <<-EOSQL > init
+CREATE KEYSPACE $KEY_SPACE WITH replication = {'class':'SimpleStrategy', 'replication_factor' : 1};
+
+EOSQL
+
+cqlsh --file=/tmp/init
+
+echo =============== KEY_SPACE CREATED ==========================
+
+#trap
+while [ "$END" == '' ]; do
+			sleep 1
+			trap "END=1" INT TERM
+done
