9c35c278c219534d936c15f9098bcc7ec5adb845
Vasily Chernov <Vasily.Chernov@jetbrains.com>
2020-02-25 18:33:36 +0300

    [db2] extending db2 11 with geospatial

--- db2/11/createschema.sh
+++ db2/11/createschema.sh
@@ -9,4 +9,18 @@ enable_explain_plan()
     su - ${DB2INSTANCE?} -c "db2 connect to ${DBNAME?} && db2 -f /var/custom/init.sql"
 }
 
+create_buffer_pool()
+{
+    echo "(*) Creating buffer pool and table space"
+    su - ${DB2INSTANCE?} -c "db2 connect to ${DBNAME?} && db2 -f /var/custom/initspatial.sql"
+}
+
+enable_spatial_operations()
+{
+    echo "(*) Enabling spatial operations feature"
+    su - ${DB2INSTANCE?} -c "/opt/ibm/db2/V11.1/bin/db2se enable_db ${DBNAME?}"
+}
+
 enable_explain_plan
+create_buffer_pool
+enable_spatial_operations
