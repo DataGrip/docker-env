950f55c9665b437461d9283f210dc170142fa136
Vasily Chernov <vasily.chernov@jetbrains.com>
2020-07-30 12:59:47 +0300

    [dbe] new init for virtual schema adapter

--- exasol/6.2/init.sql
+++ exasol/6.2/init.sql
@@ -1,6 +1,7 @@
-CREATE SCHEMA adapter;
+CREATE SCHEMA SCHEMA_FOR_VS_SCRIPT;
 
-CREATE JAVA ADAPTER SCRIPT adapter.jdbc_adapter AS
- %scriptclass com.exasol.adapter.jdbc.JdbcAdapter;
- %jar /buckets/bfsdefault/default/virtual-schema-jdbc-adapter-4.0.2.jar;
+CREATE JAVA ADAPTER SCRIPT SCHEMA_FOR_VS_SCRIPT.JDBC_ADAPTER_SCRIPT AS
+  %scriptclass com.exasol.adapter.RequestDispatcher;
+  %jar /buckets/your-bucket-fs/your-bucket/virtual-schema-dist-5.0.2-bundle-4.0.2.jar;
+  %jar /buckets/bfsdefault/default/virtual-schema-jdbc-adapter-4.0.2.jar;
 /
