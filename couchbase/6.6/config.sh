eb55cb09660732b2f8636b6c5330d2a73c461dc6
Vasily Chernov <vasily.chernov@jetbrains.com>
2020-09-09 17:22:05 +0300

    [dbe] couchbase 6.6 (indexes, quota to 512)

--- couchbase/6.6/config.sh
+++ couchbase/6.6/config.sh
@@ -12,13 +12,22 @@ while [[ $STATUS -eq 0 ]] || [[ $i -lt 30 ]]; do
 	STATUS=$(grep -r "Apache CouchDB has started on.*" /opt/couchbase/var/lib/couchbase/logs/couchdb.log | wc -l)
 done
 
-curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=256 -d indexMemoryQuota=256 -d ftsMemoryQuota=256
+curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=512 -d indexMemoryQuota=512 -d ftsMemoryQuota=512
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Cfts
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
 curl -v -X POST http://127.0.0.1:8091/pools/default/buckets \
 -u Administrator:password \
 -d name=guest \
--d ramQuotaMB=256 \
+-d ramQuotaMB=512 \
 -d bucketType=ephemeral
+
+curl -v -X POST http://127.0.0.1:8091/settings/indexes \
+-u Administrator:password \
+-d indexerThreads=0 \
+-d logLevel=info \
+-d maxRollbackPoints=5 \
+-d memorySnapshotInterval=200 \
+-d stableSnapshotInterval=5000 \
+-d storageMode=memory_optimized
 
 fg 1
