09ba656ab75cb0c7651466dc9b1084f5aff75fe1
Vasily Chernov <Vasily.Chernov@jetbrains.com>
2021-03-16 10:19:06 +0300

    [dbe] exasol 7

---
+++ exasol/7.0/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/opt/EXASuite-7/EXAClusterOS-${EXA_RE_VERSION?}/docker/exadt init-sc & /init.sh
---
+++ exasol/7.0/init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo =============== WAITING FOR EXASOL ==========================
+#waiting for exasol to start
+export STATUS=0
+i=0
+while [[ $STATUS -eq 0 ]] || [[ $i -lt 120 ]]; do
+	sleep 1
+	i=$((i+1))
+	STATUS=$(grep 'System is ready to receive client connections.' /exa/logs/logd/DWAd.log | wc -l)
+done
+
+echo =============== PUTTING ADAPTER TO DEFAULT BUCKET ==========================
+#get default bucket write password
+BW_PWD=$(grep 'write-password' /exa/etc/bucketfs.cfg_bfsdefault | sed "s/write-password = //g" | base64 -d)
+curl -X PUT -T /virtual-schema-dist-5.0.2-exasol-3.0.2.jar http://w:$BW_PWD@localhost:2580/default/virtual-schema-dist-5.0.2-exasol-3.0.2.jar
+
+if [ ! -f /exa/logs/logd/Authentication.log ]; then
+    echo =============== ADAPTER ADDED TO DEFAULT BUCKET ==========================
+fi
+
+if [ ! -z $EXASOL_SCHEMA ]; then
+	echo "EXASOL_SCHEMA: $EXASOL_SCHEMA"
+else
+	EXASOL_SCHEMA=test
+	echo "EXASOL_SCHEMA: $EXASOL_SCHEMA"
+fi
+
+#creating default schema
+echo "CREATE SCHEMA $EXASOL_SCHEMA;" | ./usr/opt/EXASuite-7/EXASolution-${EXA_RE_VERSION?}/bin/Console/exaplus -c n11 -u sys -P exasol
+
+echo =============== EXASOL STARTED ==========================
+
+#trap
+while [ "$END" == '' ]; do
+    sleep 1
+	trap "/usr/opt/EXASuite-7/EXAClusterOS-${EXA_RE_VERSION?}/devel/docker/exadt stop && END=1" INT TERM
+done
