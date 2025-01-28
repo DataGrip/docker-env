874db55ddb18c7f29b21afc8d1b02df299a671f4
Dinara Tablina <dinara.tablina@jetbrains.com>
2025-01-28 10:48:59 +0100

    correct createschema.sh in Db2 12.1

--- db2/12.1/createschema.sh
+++ db2/12.1/createschema.sh
@@ -18,16 +18,17 @@ create_buffer_pool()
 enable_spatial_operations()
 {
     echo "(*) Enabling spatial operations feature"
-    su - ${DB2INSTANCE?} -c "/opt/ibm/db2/V11.5/bin/db2se enable_db ${DBNAME?}"
+    su - ${DB2INSTANCE?} -c "/opt/ibm/db2/V12.1/bin/db2se enable_db ${DBNAME?}"
 }
 
 start_server_listener()
 {
-    echo "(*) Restarting DB2 server's listener"
+    echo "(*) Restarting Db2 server's listener"
+    su - ${DB2INSTANCE?} -c "
     db2set db2comm=tcpip
-    db2 "update dbm cfg using svcename 60000"
+    db2 'update DBM CFG using SVCENAME 60000'
     db2stop
-    db2start
+    db2start"
 }
 
 enable_explain_plan
