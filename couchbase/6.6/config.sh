9d84354a8a5b392cd54b03ecb35d0806111dde14
Vasily.Chernov <vasily.chernov@jetbrains.com>
2026-07-17 14:32:14 +0200

    More accurate Couchbase config scripts

--- couchbase/6.6/config.sh
+++ couchbase/6.6/config.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 set -m
+set -e
 
 /entrypoint.sh couchbase-server &
 
 export STATUS=0
 i=0
-while [[ $STATUS -eq 0 ]] || [[ $i -lt 30 ]]; do
+while (( $i < 90 )); do
 	sleep 1
 	i=$((i+1))
+	echo $i
 	STATUS=$(grep -r "Apache CouchDB has started on.*" /opt/couchbase/var/lib/couchbase/logs/couchdb.log | wc -l)
+	if (( $STATUS != 0 )); then
+		echo "Server has started"
+		break
+	fi
 done
+if (( $STATUS == 0 )); then
+	echo "ERROR: Couchbase server did not start within 90s" >&2
+	exit 1
+fi
 
 yes | ./opt/couchbase/bin/couchbase-cli enable-developer-preview --enable -c localhost:8091 -u Administrator -p password
 
@@ -28,16 +38,16 @@ else
 	echo "COUCH_USRPWD: $COUCH_USRPWD"
 fi
 
-curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=1024 -d indexMemoryQuota=1024 -d ftsMemoryQuota=1024
-curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Cfts
-curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
-curl -v -X POST http://127.0.0.1:8091/pools/default/buckets \
+curl -v -f -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=1024 -d indexMemoryQuota=1024 -d ftsMemoryQuota=1024
+curl -v -f http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex%2Cfts
+curl -v -f http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
+curl -v -f -X POST http://127.0.0.1:8091/pools/default/buckets \
 -u Administrator:password \
 -d name=guest \
 -d ramQuotaMB=512 \
 -d bucketType=ephemeral
 
-curl -v -X POST http://127.0.0.1:8091/settings/indexes \
+curl -v -f -X POST http://127.0.0.1:8091/settings/indexes \
 -u Administrator:password \
 -d indexerThreads=0 \
 -d logLevel=info \
@@ -46,12 +56,12 @@ curl -v -X POST http://127.0.0.1:8091/settings/indexes \
 -d stableSnapshotInterval=5000 \
 -d storageMode=plasma
 
-curl -v -X  PUT -u Administrator:password \
+curl -v -f -X  PUT -u Administrator:password \
 http://localhost:8091/settings/rbac/users/local/admin \
 -d password=$COUCH_ADMPWD \
 -d roles=admin
 
-curl -v -X  PUT -u Administrator:password \
+curl -v -f -X  PUT -u Administrator:password \
 http://localhost:8091/settings/rbac/users/local/roadmin \
 -d password=$COUCH_USRPWD \
 -d roles=ro_admin
--- couchbase/7.0/config.sh
+++ couchbase/7.0/config.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 set -m
+set -e
 
 /entrypoint.sh couchbase-server &
 
 export STATUS=0
 i=0
-while (( $i < 30 )); do
+while (( $i < 90 )); do
 	sleep 1
 	i=$((i+1))
 	echo $i
@@ -16,6 +17,10 @@ while (( $i < 30 )); do
  		break
  	fi
 done
+if (( $STATUS == 0 )); then
+	echo "ERROR: Couchbase server did not start within 90s" >&2
+	exit 1
+fi
 
 yes | ./opt/couchbase/bin/couchbase-cli enable-developer-preview --enable -c localhost:8091 -u Administrator -p password
 
@@ -35,34 +40,40 @@ fi
 
 
 # Initialize Node
-curl  -u Administrator:password -v -X POST \
+curl -v -f -u Administrator:password -X POST \
   http://localhost:8091/nodes/self/controller/settings \
-  -d 'path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&' \
-  -d 'index_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&' \
-  -d 'cbas_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&' \
-  -d 'eventing_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata&'
+  -d 'path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata' \
+  -d 'index_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata' \
+  -d 'cbas_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata' \
+  -d 'eventing_path=%2Fopt%2Fcouchbase%2Fvar%2Flib%2Fcouchbase%2Fdata'
 
 # services
-curl  -u Administrator:password -v -X POST http://localhost:8091/node/controller/setupServices \
+curl -v -f -u Administrator:password -X POST http://localhost:8091/node/controller/setupServices \
   -d 'services=kv%2Cn1ql%2Cindex%2Cfts'
 
 # Memory Quotas
-curl  -u Administrator:password -v -X POST http://localhost:8091/pools/default \
+curl -v -f -u Administrator:password -X POST http://localhost:8091/pools/default \
   -d 'memoryQuota=256' \
   -d 'indexMemoryQuota=256' \
   -d 'ftsMemoryQuota=256'
 
 # Administrator username and password
-curl  -u Administrator:password -v -X POST http://localhost:8091/settings/web \
+curl -v -f -u Administrator:password -X POST http://localhost:8091/settings/web \
   -d 'password=password&username=Administrator&port=SAME'
 
 # bucket
-curl  -u Administrator:password -v -X POST http://localhost:8091/pools/default/buckets \
-  -d 'flushEnabled=1&threadsNumber=3&replicaIndex=0&replicaNumber=0& \
-  evictionPolicy=valueOnly&ramQuotaMB=256&bucketType=membase&name=guest'
+curl -v -f -u Administrator:password -X POST http://localhost:8091/pools/default/buckets \
+  -d 'flushEnabled=1' \
+  -d 'threadsNumber=3' \
+  -d 'replicaIndex=0' \
+  -d 'replicaNumber=0' \
+  -d 'evictionPolicy=valueOnly' \
+  -d 'ramQuotaMB=256' \
+  -d 'bucketType=membase' \
+  -d 'name=guest'
 
 # indexes
-curl -v -X POST http://127.0.0.1:8091/settings/indexes \
+curl -v -f -X POST http://127.0.0.1:8091/settings/indexes \
 -u Administrator:password \
 -d indexerThreads=0 \
 -d logLevel=info \
@@ -72,13 +83,13 @@ curl -v -X POST http://127.0.0.1:8091/settings/indexes \
 -d storageMode=plasma
 
 # admin user
-curl -v -X  PUT -u Administrator:password \
+curl -v -f -X  PUT -u Administrator:password \
 http://localhost:8091/settings/rbac/users/local/admin \
 -d password=$COUCH_ADMPWD \
 -d roles=admin
 
 # read only admin
-curl -v -X  PUT -u Administrator:password \
+curl -v -f -X  PUT -u Administrator:password \
 http://localhost:8091/settings/rbac/users/local/roadmin \
 -d password=$COUCH_USRPWD \
 -d roles=ro_admin
